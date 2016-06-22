a7751f44afdaa2ebc7f4eb183503ddfdaf6cb9b8
Ryan Dale <rd01748@georgiasouthern.edu>
2016-06-21 22:57:05 -0400

    Add roster endpoint

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,11 @@ var IndexModel = require('../models/index'),
 
 module.exports = function (router) {
 
-    let model = new IndexModel();
+    let model = new IndexModel(),
+        headers = {
+            'User-Agent': 'BaseballScoresRobot/1.0 (rdale@atlatlsoftware.com)',
+            'Authorization': 'Bearer 8d11591d-a588-43b5-9dea-a68fb2b1fc39'
+        };
 
     router.get('/', function (req, res) {
         res.render('index.html', model);
@@ -14,10 +18,21 @@ module.exports = function (router) {
     router.get('/game/:eventId', function (req, res, next) {
         let options = {
             url: `https://erikberg.com/mlb/boxscore/${req.params.eventId}.json`,
-            headers: {
-                'User-Agent': 'BaseballScoresRobot/1.0 (rdale@atlatlsoftware.com)',
-                'Authorization': 'Bearer 8d11591d-a588-43b5-9dea-a68fb2b1fc39'
-            }
+            headers: headers
+        };
+
+        function callback(error, response, body) {
+            res.send(JSON.parse(body));
+            return next();
+        }
+
+        request(options, callback);
+    });
+
+    router.get('/roster/:teamId', function (req, res, next) {
+        let options = {
+            url: `https://erikberg.com/mlb/results/${req.params.teamId}.json`,
+            headers: headers
         };
 
         function callback(error, response, body) {
@@ -31,10 +46,7 @@ module.exports = function (router) {
     router.get('/games', function (req, res, next) {
         let options = {
             url: 'https://erikberg.com/mlb/events.json',
-            headers: {
-                'User-Agent': 'BaseballScoresRobot/1.0 (rdale@atlatlsoftware.com)',
-                'Authorization': 'Bearer 8d11591d-a588-43b5-9dea-a68fb2b1fc39'
-            },
+            headers: headers,
             qs: {
                 date: req.query.date,
                 sport: 'mlb'
